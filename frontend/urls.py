7acb97584b640bbfd30c4aebc89e9f89805cce0a
joucnuck-br <joucknuck_br@hotmail.com>
2024-02-29 16:54:30 -0300

    JWT added

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from frontend.views import conversion_form
+from frontend.views import conversion_form, register_form, logout, login_form
 
 urlpatterns = [
+    path('login_form/', login_form, name='login_form'),
+    path('register_form/', register_form, name='register_form'),
+    path('logout/', logout, name='logout_form'),
     path('conversion_form/', conversion_form, name='conversion_form'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,59 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
+from rest_framework.decorators import api_view
+
+from backend.forms import ConversionForm
+from backend.views import UserCreateAPIView
+from utils.authentication import LoginView
 
 
+@api_view(['GET', 'POST'])
 def conversion_form(request):
-    return render(request, 'conversion_form.html')
+    if request.method == 'GET':
+        conversion = ConversionForm()
+        return render(conversion, 'conversion_form.html')
+    else:
+        redirect('post_conversion')
+
+
+def login_form(request):
+    if request.method == 'GET':
+        return render(request, 'login_form.html')
+    elif request.method == 'POST':
+        data = {
+            "username": request.POST.get('username'),
+            "password": request.POST.get('password')
+        }
+        user = LoginView()
+        request.data = data
+        response_login = LoginView.post(user, request)
+        request_login = HttpRequest()
+        request_login.data = response_login.data
+        response_login.method = 'POST'
+        if response_login.status_code == 200:
+            return conversion_form(request_login)
+        else:
+            message = "Invalid username or password"
+            return render(request, 'login_form.html', {'messages': [message]})
+
+
+def register_form(request):
+    if request.method == 'GET':
+        return render(request, 'create_user.html')
+    elif request.method == 'POST':
+        data = {
+            "username": request.POST.get('username'),
+            "password": request.POST.get('password1'),
+            "email": request.POST.get('email'),
+            "first_name": request.POST.get('first_name'),
+            "last_name": request.POST.get('last_name')
+        }
+        user = UserCreateAPIView()
+        user.data = data
+        UserCreateAPIView.post(user, user)
+        return render(request, 'login_form.html')
+
+
+@api_view(['GET', 'POST'])
+def logout(request):
+    return render(request, 'login_form.html')
